24a6467859de080fe8d309a86eb0401257cd787b
The Knave <omandamjerenze@gmail.com>
2026-04-01 10:04:45 +0800

    Added my Coe Java Activity

---
+++ Fundamentals-Of-Programming/Midterm/Coe.java
@@ -0,0 +1,23 @@
+public class Coe {
+    public static void main(String[] args) {
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("| SAINT COLUMBAN COLLEGE                                     08/04/2024 9:39 AM          |");
+        System.out.println("| San Fransisco Pagadian City                                                            |");
+        System.out.println("| CERTIFICATE OF ENROLLMENT                                  STUDENT COPY                |");
+        System.out.println("|                                                                                        |");
+        System.out.println("| Semester/School Year: FIRST SEMESTER, 2024 - 2025          Tuition Fee:       90,000.00|");
+        System.out.println("|                                                            Student Dev't:     20,000.00|");
+        System.out.println("|                                                            Lab Fee:          100,000.00|");
+        System.out.println("| EDP Number:           30****                               Mat Fee:          100,100.00|");
+        System.out.println("| Name:                 Omandam, Jerenze Levi T.             Other Fees:           999.00|");
+        System.out.println("| Course and Year:      BSCS - 1                             Charges:                0.00|");
+        System.out.println("| Sex:                  MALE                                 Additional Charges:  122,000|");
+        System.out.println("| Date Enrolled:        08/04/2024                           Cooking Program:        0.00|");
+        System.out.println("|                                                                                        |");
+        System.out.println("|                                                            Total Change:     999,999.00|");
+        System.out.println("|                                                            Less Payments:         10.00|");
+        System.out.println("|                                                                                --------|");
+        System.out.println("|                                                            Balance:                0.00|");
+        System.out.println("------------------------------------------------------------------------------------------");
+    }
+}
