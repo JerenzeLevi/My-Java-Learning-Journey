ccd32a4c35fbdaec7c37b637ab4f86175d90dba3
The Knave <omandamjerenze@gmail.com>
2026-04-27 22:30:38 +0800

    Added a for loop activity java

---
+++ Fundamentals-Of-Programming/Prefinal/ForLoopActivity0.java
@@ -0,0 +1,19 @@
+public class ForLoopActivity0 {
+    public static void main(String[] args) {
+        System.out.println("Counting from 1 to 10:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+        }
+
+        System.out.println("Done!");
+
+        System.out.println("Countdown Sequence:");
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Number: " + i);
+        }
+
+        System.out.println("Blast off!");
+    }
+}
