ec40600a0684eb4f7c35f83194103decc758215a
The Knave <omandamjerenze@gmail.com>
2026-05-06 17:51:59 +0800

    Added Java Activity

---
+++ Intermediate-Programming/Preliminary/LeviArrays.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class LeviArrays{
+	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+		int [] numbers = {1,2,3,4,5};
+		int index = input.nextInt();
+		System.out.println(numbers[index]);
+		
+	}
+}
