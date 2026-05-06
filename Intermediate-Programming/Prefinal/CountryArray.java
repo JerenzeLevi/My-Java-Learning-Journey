aae372a2a6a7227f25d8b6dc6c1b14d5a23d30a6
The Knave <omandamjerenze@gmail.com>
2026-05-06 17:53:50 +0800

    Added a Java Activity for Prefinal

---
+++ Intermediate-Programming/Prefinal/CountryArray.java
@@ -0,0 +1,9 @@
+public class CountryArray { 
+    public static void main(String[] args) {
+        String[] countries = {"SINGAPORE", "PHILIPPINES", "JAPAN", "INDONESIA", "MALAYSIA", "TURKEY", "INDIA"};
+        
+        for (int i = 0; i < countries.length; i++) {
+            System.out.println(countries[i]);
+        }
+    }
+}
