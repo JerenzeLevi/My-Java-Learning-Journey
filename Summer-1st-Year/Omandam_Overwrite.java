b6aeaaab5198878f4d3754c3796e56ecd2a1fefd
The Knave <omandamjerenze@gmail.com>
2026-05-12 03:39:48 +0800

    Added a Java Activity

---
+++ Summer-1st-Year/Omandam_Overwrite.java
@@ -0,0 +1,9 @@
+public class Omandam_Overwrite{
+    public static void main(String[] args) {
+        int[] arrayOfNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = arrayOfNumbers.length - 1; i >= 0; i--) {
+            System.out.println(arrayOfNumbers[i]);
+        }
+    }
+}
