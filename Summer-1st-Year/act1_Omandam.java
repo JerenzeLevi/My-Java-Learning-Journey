bdfa47bfe7c82b318b601f6ee4bf1f6cfd41257d
The Knave <omandamjerenze@gmail.com>
2026-05-13 03:44:59 +0800

    Added Java Activity

---
+++ Summer-1st-Year/act1_Omandam.java
@@ -0,0 +1,25 @@
+public class act1_Omandam {
+    public static void main(String[] args) {
+        String[] arrayNames = arrayNames();
+        
+        for(String names: arrayNames){
+            System.out.println(names);
+        }
+            }
+    public static String[] arrayNames(){
+        String[] arrayNames={
+            "Lady Furina",
+            "Peruere",
+            "Mavuika",
+            "Ei",
+            "Kurumi Tokisaki",
+            "Zero Two",
+            "Miku Nakano",
+            "Nino Nakano",
+            "Arisu Sakanayagi",
+            "Honami Ichinose",
+            "Sachiko Line Amor",
+        };
+        return arrayNames;
+    }
+}
